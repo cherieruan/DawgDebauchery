a7ebe8643a2d7c24fbe250ffb21a2f0c13a61d91
Francisco Mendoza <fmendoza@uw.edu>
2017-11-29 14:19:04 -0800

    add event class

---
+++ app/src/main/java/Event.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+
+/**
+ * Class that stores all the information related to an event
+ */
+
+public class Event {
+
+    public String name;
+    public String address;
+    public Date date;
+    public String time;
+    public String description;
+
+    public Event(String address, Date date, String time, String description) {
+        this.address = address;
+        this.date = date;
+        this.time = time;
+        this.description = description;
+    }
+
+    public String toString() {
+        return name + " " + description + " on " + date + " at " + time;
+
+    }
+}
