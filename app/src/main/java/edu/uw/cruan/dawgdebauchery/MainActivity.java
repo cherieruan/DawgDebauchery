a57ef54150d6ba9984164d3abf9c8e66cf55536f
Cherie Ruan <cherie_ruan@hotmail.com>
2017-12-04 14:48:45 -0800

    Set full name on main

--- app/src/main/java/edu/uw/cruan/dawgdebauchery/MainActivity.java
+++ app/src/main/java/edu/uw/cruan/dawgdebauchery/MainActivity.java
@@ -8,11 +8,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private boolean hasEvent = true;
+    private String userUID;
+    private DatabaseReference mDatabase;
+    private String fullName = "";
+    private boolean hasName = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +33,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         Button main_view_map = (Button) this.findViewById(R.id.main_view_map);
         Button main_view_events_list = (Button) this.findViewById(R.id.main_view_events_list);
         Button main_edit_profile = (Button) this.findViewById(R.id.main_edit_profile);
         Button main_create_event = (Button) this.findViewById(R.id.main_create_event);
+
+        // Set current user
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        userUID = user.getUid();
+
+        // Grab name from database
+        mDatabase.child("Users").child(userUID).child("firstName").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (hasName) {
+                    fullName = "Welcome, " + ((String) dataSnapshot.getValue()) + " " + fullName + "!";
+                    TextView profileName = (TextView) findViewById(R.id.profile_name);
+                    profileName.setText(fullName);
+                } else {
+                    fullName += ((String) dataSnapshot.getValue()) + " ";
+                    hasName = !hasName;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+
+        mDatabase.child("Users").child(userUID).child("lastName").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                fullName = fullName + (String) dataSnapshot.getValue();
+                if (hasName) {
+                    TextView profileName = (TextView) findViewById(R.id.profile_name);
+                    profileName.setText(fullName);
+                } else {
+                    hasName = !hasName;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
 
         main_view_map.setOnClickListener(new View.OnClickListener() {
             @Override
