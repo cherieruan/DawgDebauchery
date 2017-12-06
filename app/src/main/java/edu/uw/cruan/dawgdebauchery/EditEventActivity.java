d858cf08483d93bbaec77658234d80dfaa69e294
Joshua <kkjoboy@gmail.com>
2017-12-06 13:13:26 -0800

    Removed a bunch of action bars

--- app/src/main/java/edu/uw/cruan/dawgdebauchery/EditEventActivity.java
+++ app/src/main/java/edu/uw/cruan/dawgdebauchery/EditEventActivity.java
@@ -32,8 +32,8 @@ public class EditEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_event);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
--- app/src/main/java/edu/uw/cruan/dawgdebauchery/RegistrationActivity.java
+++ app/src/main/java/edu/uw/cruan/dawgdebauchery/RegistrationActivity.java
@@ -28,8 +28,8 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
